df12b82cdb7b1c98ccd62d8664767b7be009c86c
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-31 21:02:03 -0300

    atualizacao

---
+++ js/calcular.js
@@ -0,0 +1,9 @@
+let name =document.getElementById("name")
+let nota1 = document.getElementById("nota1")
+let nota2 = document.getElementById("nota2")
+let nota3 = document.getElementById("nota3")
+let nota4 =document.getElementById("nota4")
+
+(function calculoMedia(){
+    
+})()
