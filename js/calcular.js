82e2616d4854720ee2c00e4b694c243a72b43851
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-31 21:38:29 -0300

    att

--- js/calcular.js
+++ js/calcular.js
@@ -1,9 +1,26 @@
-let name =document.getElementById("name")
-let nota1 = document.getElementById("nota1")
-let nota2 = document.getElementById("nota2")
-let nota3 = document.getElementById("nota3")
-let nota4 =document.getElementById("nota4")
+let name = document.getElementById("name")
 
-(function calculoMedia(){
+function calcularMedia(nota1,nota2, nota3, nota4){
+    // verificando se os inputs são númericos
+    if(isNaN(nota1) || sNaN(nota2) || sNaN(nota3)|| sNaN(nota4)){
+        return "Por favor, inserir valores válidos"
+    }
+     var media = nota1 + nota2 + nota3 + nota4;
     
-})()
+    return media;
+
+}
+
+
+function calcular(){
+    // Aqui recebemos os valores do input
+    let nota1 = parseFloat(document.getElementById("nota1").value)
+    let nota2 = parseFloat(document.getElementById("nota2").value)
+    let nota3 = parseFloat(document.getElementById("nota3").value)
+    let nota4 = parseFloat(document.getElementById("nota4").value)
+
+    var media = calcularMedia(nota1, nota2, nota3, nota4)
+
+    //exibir o resultado
+    document.getElementById("media").innerText = `${media}`
+}
