151bdc9e4224303c76485101fd4d945edd171f92
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-06-02 18:16:41 -0300

    Update calcular.js

--- js/calcular.js
+++ js/calcular.js
@@ -3,6 +3,11 @@ let thsHeader = document.querySelector("th")
 let thAlunosNotas = document.querySelector("[aluno-nota]") 
 const indices_notas = {}
 
-Array.from(thAlunosNotas).forEach(function(){
+Array.from(thAlunosNotas).forEach(function(th){
     let prop = th.getAttibute("aluno-nota")
+    indices_notas[prop] = pegarIndice(prop)
 })
+
+function pegarIndice(indice){
+    const th = trHeader.querySelector(`[aluno-nota]="${indice}"`)
+}
