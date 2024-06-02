c60f7703859b08b6ee4ac78b9fdb73c841bfc8eb
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-06-02 18:09:45 -0300

    Update calcular.js

--- js/calcular.js
+++ js/calcular.js
@@ -2,3 +2,7 @@ let trHeader = document.querySelector('thead tr')
 let thsHeader = document.querySelector("th")
 let thAlunosNotas = document.querySelector("[aluno-nota]") 
 const indices_notas = {}
+
+Array.from(thAlunosNotas).forEach(function(){
+    let prop = th.getAttibute("aluno-nota")
+})
